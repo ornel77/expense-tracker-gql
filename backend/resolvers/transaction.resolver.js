625750e555d3d2bfbe89274688cd6e907a760cec
ornel77 <t.ornel@yahoo.fr>
2024-03-02 23:33:19 +0100

    complete transaction resolver

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,10 +1,74 @@
+import Transaction from '../models/transaction.model';
+
 const transactionResolver = {
-    Query: {
+  Query: {
+    transactions: async (_, _, context) => {
+      try {
+        if (!context.getUser()) {
+          throw new Error('Unauthorized');
+        }
+        const userId = await context.getUser()._id;
 
+        const transactions = await Transaction.find({ userId });
+        console.log(transactions);
+        return transactions;
+      } catch (error) {
+        console.error('Error getting transactions: ', err);
+        throw new Error('Error getting transactions');
+      }
     },
-    Mutation: {
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (error) {
+        console.error('Error getting one transaction: ', err);
+        throw new Error('Error getting one transaction');
+      }
+    },
+    // TODO
+    categoryStatistics: () => {},
+  },
+  Mutation: {
+    createTransaction: async (_, { input }, context) => {
+      try {
+        const newTransaction = new Transaction({
+          ...input,
+          userId: context.getUser()._id,
+        });
+        await newTransaction.save();
+        return newTransaction;
+      } catch (error) {
+        console.error('Error in createTransaction: ', err);
+        throw new Error(err.message || 'error creating transaction');
+      }
+    },
+    updateTransaction: async (_, { input }, context) => {
+      try {
+        const updatedTransaction = await Transaction.findByIdAndUpdate(
+          input.transactionId,
+          input,
+          { new: true }
+        );
+        return updatedTransaction;
+      } catch (error) {
+        console.error('Error in updateTransaction: ', err);
+        throw new Error(err.message || 'Error in updateTransaction');
+      }
+    },
+    deleteTransaction: async (_, { transactionId }, context) => {
+      try {
+        const deletedTransaction = await Transaction.findByIdAndDelete(
+          transactionId
+        );
+        return deletedTransaction;
+      } catch (error) {
+        console.error('Error in deleteTransaction: ', err);
+        throw new Error(err.message || 'Internal server error');
+      }
+    },
+  },
+  //   TODO => ADD transaction/user relation
+};
 
-    }
-}
-
-export default transactionResolver
+export default transactionResolver;
