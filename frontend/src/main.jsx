c81881fd526c05c962406650e3b5d66fd15e7ae4
ornel77 <t.ornel@yahoo.fr>
2024-03-03 01:22:41 +0100

    add ui and harcoded

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import GridBackground from './ui/GridBackground.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+			<GridBackground>
+				<App />
+			</GridBackground>
+		</BrowserRouter>
 )
